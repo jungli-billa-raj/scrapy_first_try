47200cf7ed997e446e7cf62c43b7bf1a3d09a43e
Kumar Raj <rajbhkseries@gmail.com>
2026-01-02 16:41:35 +0530

    step 3 finished. created an item BookTrackerItem in items.py and returned structured data

--- book_tracker/spiders/books.py
+++ book_tracker/spiders/books.py
@@ -1,4 +1,5 @@
 import scrapy
+from book_tracker.items import BookTrackerItem
 
 
 class BooksSpider(scrapy.Spider):
@@ -8,3 +9,9 @@ class BooksSpider(scrapy.Spider):
 
     def parse(self, response):
         self.logger.info("Spider is running at URL=%s", response.url)
+        item = BookTrackerItem()
+        # .get() is safe. will return None if nothing is found. Cool
+        item["url"] = response.url
+        item["title"] = response.css("title::text").get()
+
+        yield item
